628a870689d5996b0526de2930a19c35c1d4a76d
Saiful Shaik <Saifulshaik47@gmail.com>
2025-05-20 21:49:02 -0400

    Buttons Added to Title Screen
    Features:
    - Clickable buttons.
    - Hover Effect on each Button.
    - Button Images added to Images folder in button images.

---
+++ QuitButton.java
@@ -0,0 +1,65 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class QuitButton here.
+ * 
+ * @author Saiful Shaik
+ * @version May 20, 2025.
+ */
+public class QuitButton extends Actor {
+    private GreenfootImage baseImage;
+    private int fadeLevel = 0;
+    private final int maxFade = 80;
+    private final int fadeSpeed = 5;
+
+    public QuitButton() {
+        baseImage = new GreenfootImage("images/buttonImages/button0.png");
+
+        int targetWidth = 200;
+        int targetHeight = (int)(baseImage.getHeight() * (200.0 / baseImage.getWidth()));
+        baseImage.scale(targetWidth, targetHeight);
+
+        setImage(new GreenfootImage(baseImage));
+    }
+
+    public void act() {
+        boolean mouseOver = isMouseOverAccurate();
+
+        if (mouseOver && fadeLevel < maxFade) {
+            fadeLevel += fadeSpeed;
+        } else if (!mouseOver && fadeLevel > 0) {
+            fadeLevel -= fadeSpeed;
+        }
+
+        fadeLevel = Math.max(0, Math.min(maxFade, fadeLevel));
+        updateImageWithFade(fadeLevel);
+
+        if (Greenfoot.mouseClicked(this)) {
+            // Quit the Game.
+        }
+    }
+
+    private void updateImageWithFade(int alpha) {
+        GreenfootImage img = new GreenfootImage(baseImage);
+        if (alpha > 0) {
+            img.setColor(new Color(0, 0, 0, alpha));
+            img.fillRect(0, 0, img.getWidth(), img.getHeight());
+        }
+        setImage(img);
+    }
+
+    private boolean isMouseOverAccurate() {
+        MouseInfo mouse = Greenfoot.getMouseInfo();
+        if (mouse == null) return false;
+
+        int mx = mouse.getX();
+        int my = mouse.getY();
+        int x = getX();
+        int y = getY();
+        int halfW = getImage().getWidth() / 2;
+        int halfH = getImage().getHeight() / 2;
+
+        return (mx >= x - halfW && mx <= x + halfW &&
+                my >= y - halfH && my <= y + halfH);
+    }
+}
--- Titlescreen.java
+++ Titlescreen.java
@@ -1,4 +1,4 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
 /**
  * Write a description of class Titlescreen here.
@@ -6,16 +6,38 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Titlescreen extends World
-{
-
-    /**
-     * Constructor for objects of class Titlescreen.
-     * 
-     */
-    public Titlescreen()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(960, 540, 1); 
+public class Titlescreen extends World {
+    Label playTitle = new Label("Play Game", 30);
+    Label optionTitle = new Label("Options", 30);
+    Label helpTitle = new Label("Help", 30);
+    Label quitTitle = new Label("Quit", 30);
+    
+    private int buttonOffset = 80;
+    private int verticleOffset = 10;
+    public Titlescreen() {    
+        super(960, 540, 1);
+        
+        // Play Button Label
+        PlayButton playButton = new PlayButton();
+        addObject(playButton, getWidth() / 2, (getHeight() / 2) - verticleOffset);
+        addObject(playTitle, getWidth() / 2, (getHeight() / 2) - verticleOffset);
+        
+        
+        // Option Button Label
+        OptionButton optionButton = new OptionButton();
+        addObject(optionButton, getWidth() / 2, (getHeight() / 2) - verticleOffset + buttonOffset);
+        addObject(optionTitle, getWidth() / 2, (getHeight() / 2) - verticleOffset + buttonOffset);
+        
+        
+        // Help Button Label
+        HelpButton helpButton = new HelpButton();
+        addObject(helpButton, getWidth() / 2, (getHeight() / 2) - verticleOffset + (buttonOffset*2));
+        addObject(helpTitle, getWidth() / 2, (getHeight() / 2) - verticleOffset + (buttonOffset*2));
+        
+        
+        // Quit Button Label
+        QuitButton quitButton = new QuitButton();
+        addObject(quitButton, getWidth() / 2, (getHeight() / 2) - verticleOffset + (buttonOffset*3));
+        addObject(quitTitle, getWidth() / 2, (getHeight() / 2) - verticleOffset + (buttonOffset*3));
     }
 }
