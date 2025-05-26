47f8e4f0f6eb9856883e0e7a67c8cd121081ccef
33de9r <chenjiayu2033@gmail.com>
2025-05-26 14:27:06 -0400

    Add Stone tiles to level 1

--- Level1.java
+++ Level1.java
@@ -9,12 +9,27 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Level1 extends World
 {
     private static final boolean BOUNDED = true;
+    private static final int IMAGE_OVERLAP = 30;
+    private static final int STARTING_X = -30;
+    
+    private final int groundY = getHeight() - 10;
     
     public Level1(int width, int height)
     {    
         super(width, height, 2, BOUNDED); 
         
+        // add stone tiles
+        addStoneTiles();
     }
     
-    
+    private void addStoneTiles() {
+        Stone sampleStone = new Stone();
+        int tileWidth = sampleStone.getTargetWidth() - IMAGE_OVERLAP;
+        int worldWidth = getWidth();
+        int tileCount = (worldWidth - STARTING_X) / tileWidth + 2;
+        for (int i = 0; i < tileCount - 10; i++) {
+            int x = STARTING_X + i * tileWidth;
+            addObject(new Stone(), x, groundY);
+        }
+    }
 }
--- Stone.java
+++ Stone.java
@@ -8,12 +8,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Stone extends Base
 {
-    /**
-     * Act - do whatever the Stone wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private GreenfootImage baseImage;
+    private static final int targetWidth = 30;
+    
+    public Stone() {
+        baseImage = new GreenfootImage("images/stoneTiles.png");
+
+        int targetHeight = (int)(baseImage.getHeight() * ((double) targetWidth / baseImage.getWidth()));
+        baseImage.scale(targetWidth, targetHeight);
+
+        setImage(new GreenfootImage(baseImage));
+    }
+    
     public void act()
     {
         // Add your action code here.
+    }
+    
+    public int getTargetWidth() {
+        return targetWidth;
     }
 }
