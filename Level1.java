b204bd404e219af0b25b840ebb497a80855588ba
33de9r <chenjiayu2033@gmail.com>
2025-05-26 14:16:46 -0400

    Level 1 images

--- Level1.java
+++ Level1.java
@@ -3,19 +3,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Level1 here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (your name) Jiayu Chen
+ * @version (a version number or a date) 05/26/25
  */
 public class Level1 extends World
 {
-
-    /**
-     * Constructor for objects of class Level1.
-     * 
-     */
-    public Level1()
+    private static final boolean BOUNDED = true;
+    
+    public Level1(int width, int height)
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(width, height, 2, BOUNDED); 
+        
     }
+    
+    
 }
