c5ba4b0be46fe2038ea8dede08a060ae8d60367b
33de9r <chenjiayu2033@gmail.com>
2025-06-02 13:52:18 -0400

    Revert "added endscreen"
    This reverts commit 453ec25f4c50d94073ae7fe1b2e9e281f143b07a.

--- Level1.java
+++ Level1.java
@@ -104,12 +104,4 @@ public class Level1 extends World {
     public Player getPlayer() {
         return player;
     }
-    
-    public int getScreenWidth() {
-        return screenWidth;
-    }
-    
-    public int getScreenHeight() {
-        return screenHeight;
-    }
 }
