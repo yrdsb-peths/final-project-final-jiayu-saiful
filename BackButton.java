a1eb1462728629a599e8de01732f8d6ec47edf0f
33de9r <chenjiayu2033@gmail.com>
2025-06-06 14:38:28 -0400

    update BackButton class

--- BackButton.java
+++ BackButton.java
@@ -9,6 +9,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class BackButton extends Actor
 {
     private GreenfootImage baseImage;
+    private int fadeLevel = 0;
+    private final int maxFade = 80;
+    private final int fadeSpeed = 5;
     
     public BackButton() {
         baseImage = new GreenfootImage("images/backButton.png");
@@ -22,10 +25,45 @@ public class BackButton extends Actor
     
     public void act()
     {
+        boolean mouseOver = isMouseOverAccurate();
+
+        if (mouseOver && fadeLevel < maxFade) {
+            fadeLevel += fadeSpeed;
+        } else if (!mouseOver && fadeLevel > 0) {
+            fadeLevel -= fadeSpeed;
+        }
+
+        fadeLevel = Math.max(0, Math.min(maxFade, fadeLevel));
+        updateImageWithFade(fadeLevel);
+
         if (Greenfoot.mouseClicked(this)) {
             // Go back to Titlescreen
             Titlescreen titlescreen = new Titlescreen();
             Greenfoot.setWorld(titlescreen);
         }
+    }
+    
+    private void updateImageWithFade(int alpha) {
+        GreenfootImage img = new GreenfootImage(baseImage);
+        if (alpha > 0) {
+            img.setColor(new Color(0, 0, 0, alpha));
+            img.fillRect(0, 0, img.getWidth(), img.getHeight());
+        }
+        setImage(img);
+    }
+
+    private boolean isMouseOverAccurate() {
+        MouseInfo mouse = Greenfoot.getMouseInfo();
+        if (mouse == null) return false;
+
+        int mx = mouse.getX();
+        int my = mouse.getY();
+        int x = getX();
+        int y = getY();
+        int halfW = getImage().getWidth() / 2;
+        int halfH = getImage().getHeight() / 2;
+
+        return (mx >= x - halfW && mx <= x + halfW &&
+                my >= y - halfH && my <= y + halfH);
     }
 }
